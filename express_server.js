678d5e6e320fbe8c947167fce4b472b9519b2e77
Ethan Wong <ethanwong98@yahoo.ca>
2023-01-25 04:16:10 -0800

    added redirection from short URL to long URL

--- express_server.js
+++ express_server.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
+
+
+
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-function generateRandomString(){
-  return Math.random().toString(36).substring(2, 8)
-}
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.use(express.urlencoded({ extended: true }));
+function generateRandomString(){
+  return Math.random().toString(36).substring(2, 8)
+}
 
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
+
 app.get('/urls.json', (req, res) => {
   res.send(urlDatabase);
 });
+
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
+
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
@@ -42,6 +48,11 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+ 
+    res.redirect(urlDatabase[req.params.shortURL])
+});
 
 
 app.listen(PORT, () => {
