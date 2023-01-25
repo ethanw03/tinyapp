eef1d42fab41438e2b212e142a8d6db58dfec933
Ethan Wong <ethanwong98@yahoo.ca>
2023-01-25 04:36:08 -0800

    added routing for deletion of urls

--- express_server.js
+++ express_server.js
@@ -48,6 +48,11 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+})
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
  
