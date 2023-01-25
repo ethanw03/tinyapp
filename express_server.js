494ce2df27683e2859cbe08b4e20876ffdb15f15
Ethan Wong <ethanwong98@yahoo.ca>
2023-01-25 03:48:36 -0800

    added GET route to expressServer with template and added post route to recieve form submission

--- express_server.js
+++ express_server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
 app.set('view engine', 'ejs');
+
+function generateRandomString(){
+  return Math.random().toString(36).substring(2, 8)
+}
+ console.log(generateRandomString())
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
@@ -20,9 +29,18 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
