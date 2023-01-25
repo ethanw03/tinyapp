90ad3470a61c768dea112dd10c543eba949edba0
Ethan Wong <ethanwong98@yahoo.ca>
2023-01-25 03:57:49 -0800

    updated to save key value pair when post requests to urls

--- express_server.js
+++ express_server.js
@@ -6,8 +6,6 @@ app.set('view engine', 'ejs');
 function generateRandomString(){
   return Math.random().toString(36).substring(2, 8)
 }
- console.log(generateRandomString())
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -29,6 +27,12 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`)
+});
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -38,10 +42,7 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
