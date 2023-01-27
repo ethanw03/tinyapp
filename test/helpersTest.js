9becba66324bb41f97d41fefcb83c6577088678e
Ethan Wong <ethanwong98@yahoo.ca>
2023-01-27 13:56:45 -0800

    added mocha and chai as dependencies for test

---
+++ test/helpersTest.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+
+const { findEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('findEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = findEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.equal(user, testUsers.userRandomID )
+  });
+
+  it('should return undefined when passed a email that is not registered', function() {
+    const user = findEmail("ethan@ethan.com", testUsers);
+    assert.equal(user, undefined);
+  });
+});
